f57225f28dc714e71ac8a68201d6c219d5212d54
chaltenio <dev@chalten.io>
2020-11-08 14:30:05 +1300

    Added getProduct

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http')
-const { getProducts } = require('./controllers/productControllers')
+const { getProducts, getProduct } = require('./controllers/productControllers')
 
 const server = http.createServer( (req, res)=> {
     if(req.url === '/api/products' && req.method === 'GET'){
         getProducts(req, res)
+    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET')
+    {
+        const id = req.url.split('/')[3]
+        getProduct(req, res, id)
     } else {
         res.writeHead(404, {'Content-Type': "application/json"})
         res.end(JSON.stringify({message: 'Route Not Found'}))
